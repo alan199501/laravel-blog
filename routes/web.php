f6f86bf2e79c174fa43f93471247b780d01adb39
alan <97301528+alan199501@users.noreply.github.com>
2025-06-27 00:31:45 +0800

    feat: Integrate Filament for admin panel and remove old admin code

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\Admin\PostController as AdminPostController;
+
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('posts', AdminPostController::class);
-});
+
